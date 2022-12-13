e6ac18ef3c25a6569440bcf5087874e57a0417a8
Rohit Tokala <rtokala@purdue.edu>
2022-12-13 01:00:52 -0500

    Add article styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import { categories } from "../constants";
 import fetchNews from "../utils/fetchNews";
+import NewsList from "./NewsList";
+import response from "../response.json";
 
 async function HomePage() {
   // fetch the news data
-  const news: NewsResponse = await fetchNews(categories.join(","));
+  const news: NewsResponse =
+    response || (await fetchNews(categories.join(",")));
 
-  console.log(news);
-
-  return <div>{/* NewsList news */}</div>;
+  return (
+    <div>
+      <NewsList news={news} />
+    </div>
+  );
 }
 
 export default HomePage;
