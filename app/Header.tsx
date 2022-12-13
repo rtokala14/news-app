9f23b4fdef9d35695b6324dc2b979b755c47ee54
Rohit Tokala <rtokala@purdue.edu>
2022-12-13 01:21:10 -0500

    Add dark mode button functionality

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,5 +1,6 @@
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import DarkModeBtn from "./DarkModeBtn";
 import NavLinks from "./NavLinks";
 import SearchBox from "./SearchBox";
 
@@ -19,8 +20,7 @@ function Header() {
         </Link>
 
         <div className=" flex items-center justify-end space-x-2">
-          {/* Dark Mode Button */}
-
+          <DarkModeBtn />
           <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800">
             Subscribe Now
           </button>
